8121a01a163b400e77e46c01304b3c9bb18224a0
PintilieGeani <pintilie.geany@yahoo.it>
2025-04-19 15:10:49 +0200

    project-done

--- js/script.js
+++ js/script.js
@@ -30,8 +30,30 @@ if(presente === true){
     console.log(`La vostra mail ${mailUtente} non è presente nella lista`)
 }
 
-
+console.log("- - - - - - - - - - - - - - - - ");
 
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Risoluzione:
+
+// Raccolta dati:
+
+let giocatore = Math.floor(Math.random() * 6 + 1 )
+console.log(`Il Giocatore tira il dado e ottiene: ${giocatore}`)
+let computer = Math.floor(Math.random() * 6 + 1 );
+console.log(`Il Computer tira il dado e ottiene: ${computer}`)
+let vincitore;
+
+// Risoluzione logica
+if(giocatore > computer){
+    vincitore = "Vince il giocatore"
+}if(giocatore < computer){
+    vincitore = "Vince il Computer"
+}if(giocatore === computer){
+    vincitore = "Pareggio"
+}
+
+// Output
+console.log(vincitore)
